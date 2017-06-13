9e3e673f1357817a2516ec7212f89cf214fe5c52
rburkhardt <rburkhardt@rodericks-mbp.hsd1.ca.comcast.net>
2017-06-12 21:55:06 -0700

    initialization

---
+++ src/java/data/DBUtil.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Roderick Burkhardt
+ */
+public class DBUtil
+{
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("movie_ticketsPU");
+    
+    public static EntityManagerFactory getEmFactory() {
+        return emf;
+    }
+}
