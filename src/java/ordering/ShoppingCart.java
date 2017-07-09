397334d9ac5e63b4992e7a8058fb5e20a7c03a40
Roderick Burkhardt <roderick.burkhardt@sandc.com>
2017-07-09 08:35:35 -0700

    Add ShoppingCart Class and associated MovieTicketOrder Class. ShoppingCart Class contains an ArrayList of MovieTicketOrders to hold the order of the customer while they are logged in and until the check out. MovieTicketOrder class contains the movie_id, the date of which the movie tickets are for, the number of adult and children tickets ordered. This Object can be created and added to the customers cart so that they can order tickets for multiple movies and/or dates in one order. Included in this commit is the test for these classes as while. The test are incomplete at this time.

---
+++ src/java/ordering/ShoppingCart.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordering;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Roderick Burkhardt
+ */
+public class ShoppingCart
+{
+    private final ArrayList<MovieTicketOrder> cart;
+    
+    public ShoppingCart()
+    {
+        cart = new ArrayList<>();
+    }
+    
+    public boolean addMovieToCart( MovieTicketOrder movieTicketOrder )
+    {
+        cart.add( movieTicketOrder );
+        return true;
+    }
+    
+    public MovieTicketOrder removeMovieFromCart()
+    {
+        return null;
+    }
+    
+    public boolean emptyCart()
+    {
+        for ( int i = 0; i < cart.size(); i++ )
+            cart.remove(i);
+        
+        return cart.isEmpty();
+    }
+
+    @Override
+    public String toString()
+    {
+        String rtrnString;
+        
+        if ( !cart.isEmpty() )
+        {
+            rtrnString = "Shopping Cart contains:\n";
+            for (MovieTicketOrder item : cart )
+            {
+                rtrnString += item.toString();
+            }
+        }
+        else
+        {
+            rtrnString = "Shopping Cart is Empty";
+        }
+        
+        return rtrnString;
+    }
+    
+    
+    
+}
+
